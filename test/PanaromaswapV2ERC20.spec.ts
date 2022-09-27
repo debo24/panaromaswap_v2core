30cceaa0dfe2244aa0b2cf440399b4f86614abdd
debo24 <71261693+debo24@users.noreply.github.com>
2022-09-27 13:24:50 +0530

    Update PanaromaswapV2ERC20.spec.ts

--- test/PanaromaswapV2ERC20.spec.ts
+++ test/PanaromaswapV2ERC20.spec.ts
@@ -30,7 +30,7 @@ describe('PanaromaswapV2ERC20', () => {
   it('name, symbol, decimals, totalSupply, balanceOf, DOMAIN_SEPARATOR, PERMIT_TYPEHASH', async () => {
     const name = await token.name()
     expect(name).to.eq('Panaromaswap V2')
-    expect(await token.symbol()).to.eq('UNI-V2')
+    expect(await token.symbol()).to.eq('PANAROMA-V2')
     expect(await token.decimals()).to.eq(18)
     expect(await token.totalSupply()).to.eq(TOTAL_SUPPLY)
     expect(await token.balanceOf(wallet.address)).to.eq(TOTAL_SUPPLY)
